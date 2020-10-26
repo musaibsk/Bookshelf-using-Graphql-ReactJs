a1aafa2e586cf70b253b1bbd08aeb87abaaef077
musaibsk <39944815+musaibsk@users.noreply.github.com>
2020-10-26 21:24:10 +0530

    Author nested in books - GraphQLList implemented

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,13 +1,22 @@
 const graphQl = require("graphql");
 const _= require("lodash");
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt } = graphQl;
+const { GraphQLObjectType, 
+  GraphQLString, 
+  GraphQLSchema, 
+  GraphQLID, 
+  GraphQLInt,
+  GraphQLList
+} = graphQl;
 
 //dummy data
 var books = [
-  { name: "Name of the wind", genre: "Fantasy", id: "1" },
-  { name: "The Final Empire", genre: "Fantasy", id: "2" },
-  { name: "The Long Earth", genre: "Sci-Fi", id: "3" },
+  { name: "Name of the wind", genre: "Fantasy", id: "1", authorId: '1' },
+  { name: "The Final Empire", genre: "Fantasy", id: "2", authorId: '2' },
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3", authorId: '3' },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+  { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
+  { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' },
 ];
 
 var authors = [
@@ -22,6 +31,13 @@ const BookType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        console.log(parent);
+        return _.find(authors, {id: parent.authorId})
+      }
+    }
   }),
 });
 
@@ -30,7 +46,13 @@ const AuthorType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args){
+        return _.filter(books, {authorId: parent.id})
+      }
+    }
   })
 })
 
