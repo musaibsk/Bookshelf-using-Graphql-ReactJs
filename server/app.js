44878747c90c88989552c85a6f3213aec6588bb6
musaibsk <39944815+musaibsk@users.noreply.github.com>
2020-11-20 02:11:35 +0530

    Mongodb connected using mongoose and done the setup

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -8,6 +9,18 @@ app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }))
+
+const uri = `mongodb+srv://musaibdbuser:musaibdb@321@cluster0.ej0zh.mongodb.net/bookshelfdb?retryWrites=true&w=majority`
+const options = { useNewUrlParser: true, useUnifiedTopology: true }
+
+// mongoose.connect(`mongodb+srv://musaibdbuser:musaibdb@321@cluster0.ej0zh.mongodb.net/bookshelfdb?retryWrites=true&w=majority`, {useNewUrlParser: true}, {useUnifiedTopology: true})
+
+mongoose
+  .connect(uri, options)
+  .then(() => (console.log("Server is running with database")))
+  .catch(error => {
+    throw error
+  }) 
 
 app.listen(4000, () => {
     console.log('listerning to port 4000')
